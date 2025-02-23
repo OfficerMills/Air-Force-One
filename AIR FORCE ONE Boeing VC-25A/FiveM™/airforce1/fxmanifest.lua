a92a30eada55fcce37913120e6d038ceedc074fc
Officer Mills <officermills641@gmail.com>
2025-02-22 20:04:23 -0600

    Initial commit - Air Force One
    FiveM & Single Player Ready, Drag & Drop Boeing VC-25A "Air Force One" airplane with finished walk-in interior.

---
+++ AIR FORCE ONE Boeing VC-25A/FiveM™/airforce1/fxmanifest.lua
@@ -0,0 +1,37 @@
+--[[
+.______     ______    _______  __  .__   __.   _______    ____    ____  ______       ___    _____       ___      
+|   _  \   /  __  \  |   ____||  | |  \ |  |  /  _____|   \   \  /   / /      |     |__ \  | ____|     /   \     
+|  |_)  | |  |  |  | |  |__   |  | |   \|  | |  |  __      \   \/   / |  ,----' ______ ) | | |__      /  ^  \    
+|   _  <  |  |  |  | |   __|  |  | |  . `  | |  | |_ |      \      /  |  |     |______/ /  |___ \    /  /_\  \   
+|  |_)  | |  `--'  | |  |____ |  | |  |\   | |  |__| |       \    /   |  `----.      / /_   ___) |  /  _____  \  
+|______/   \______/  |_______||__| |__| \__|  \______|        \__/     \______|     |____| |____/  /__/     \__\
+--]]
+
+--[[      _        ______                    ____             
+    /\   (_)      |  ____|                  / __ \            
+   /  \   _ _ __  | |__ ___  _ __ ___ ___  | |  | |_ __   ___ 
+  / /\ \ | | '__| |  __/ _ \| '__/ __/ _ \ | |  | | '_ \ / _ \
+ / ____ \| | |    | | | (_) | | | (_|  __/ | |__| | | | |  __/
+/_/    \_\_|_|    |_|  \___/|_|  \___\___|  \____/|_| |_|\___| 
+--]]
+
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'SkylineGTRFreak & OfficerMills'
+description 'Boeing VC-25A "Air Force One" with viewable interior. FiveM & Single Player ready.'
+version '1.0.1'
+
+files {
+
+  'data/vehicles.meta',
+  'data/carvariations.meta',
+  'data/handling.meta',
+  'data/vehiclelayouts.meta'
+}
+
+
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/vehiclelayouts.meta'
